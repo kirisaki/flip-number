00438f81b456b65f828a232cbb6861d68bb98052
Akihito KIRISAKI <kirisaki@klaraworks.net>
2020-03-04 15:01:56 +0900

    Initial commit

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+/** @jsx jsx */
+import { jsx } from '@emotion/core'
+import * as React from 'react'
+
+export const App: React.FC = () => {
+  return (
+    <main>
+      <h1 css={{color: 'red'}}>nyaan</h1>
+    </main>
+  )
+}
