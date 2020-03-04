73ac1a4d12ad0fa16d62a8add46aad39e4753db8
Akihito KIRISAKI <kirisaki@klaraworks.net>
2020-03-04 15:35:56 +0900

    feat: Styling

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,9 @@
-/** @jsx jsx */
-import { jsx } from '@emotion/core'
 import * as React from 'react'
 
 export const App: React.FC = () => {
   return (
     <main>
-      <h1 css={{color: 'red'}}>nyaan</h1>
+      <h1>nyaan</h1>
     </main>
   )
 }
